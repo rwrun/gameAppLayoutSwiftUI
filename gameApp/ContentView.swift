a38ea500470ccddde80f3e81e294eac16465802f
rwrun <islamrun@gmail.com>
2025-06-04 19:19:55 +0500

    Initial Commit

---
+++ gameApp/ContentView.swift
@@ -0,0 +1,19 @@
+//error nil
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gameApp/gameAppApp.swift
@@ -0,0 +1,12 @@
+//error nil
+
+import SwiftUI
+
+@main
+struct gameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
